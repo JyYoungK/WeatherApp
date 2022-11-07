d387718531a35e422c3970434944ba0bdc368aa5
JyYoungK <junyoung@my.yorku.ca>
2022-11-07 00:17:15 -0500

    Dropdown transition

--- script.js
+++ script.js
@@ -1,20 +1,13 @@
 // --------------------------  Modal -------------------------- //
 var modal = document.getElementById("myModal");
-
-// Get the button that opens the modal
 var btn = document.getElementById("forecastButton");
-
-// Get the <span> element that closes the modal
 var span = document.getElementsByClassName("close")[0];
-
 var selectedCity;
 
-// When the user clicks the button, open the modal
 btn.onclick = function () {
   modal.style.display = "block";
 };
 
-// When the user clicks on <span> (x), close the modal
 span.onclick = function () {
   modal.style.display = "none";
 };
@@ -24,12 +17,6 @@ dropdown.onclick = function () {
   dropdown.classList.toggle("active");
 };
 
-// When the user clicks anywhere outside of the modal, close it
-window.onclick = function (event) {
-  if (event.target == modal) {
-    modal.style.display = "none";
-  }
-};
 // --------------------------  Modal -------------------------- //
 // ------------------------  Fetch API ------------------------ //
 function display(city) {
